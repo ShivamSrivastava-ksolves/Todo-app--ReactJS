0954b2e1a5ac634d8a5cd126bdfab743fa7dafbf
shivam <shivam.srivastava@ksolves.com>
2023-10-26 16:29:50 +0530

    todoapp with routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import "./App.css"
+import Todoinput from "./components/Todoinput"
+import Todolist from "./components/TodoList"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from './components/About'
+import Contact from './components/Contact'
+import {Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+export default function App() {
+  const [listTodo,setListTodo] = useState([]);
+  let addList = (inputText)=>{
+    setListTodo([...listTodo,inputText]);
+  }
+
+  const deleteListItem = (key)=>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1)
+    setListTodo([...newListTodo])
+  }
+  return (<BrowserRouter>
+
+  <Routes>
+       <Route path="/about" element={<About/>}/>
+       <Route path="/contact" element={<Contact/>}/>
+  </Routes>
+ <header>
+  <nav>
+    <ul>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+  </header>
+
+
+  <div className="main-container">
+    <div className="center-container">
+    
+    
+
+      <Todoinput addList={addList}/>
+      <br/>
+      <hr/>
+      <br/>
+      <h1 className="app-heading">Your Todo</h1>
+      <br/>
+      <br/>
+      {listTodo.map((listItem,i)=>{
+        return(
+          <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
+        )
+      })}
+      
     </div>
-  );
+  </div>
+  </BrowserRouter>  )
 }
-
-export default App;
